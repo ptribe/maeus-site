ca1e46cba1d8584eacd75fabb6c4d5e2938019eb
Philip Tribe <philip.tribe@gmail.com>
2015-05-11 09:04:13 -0400

    only passes current song ID

--- app/assets/js/components/audioPlayer/component.js
+++ app/assets/js/components/audioPlayer/component.js
@@ -4,13 +4,12 @@ angular.module('audioPlayer-directive', [])
             restrict: 'E',
             transclude: true,
             scope: {},
-            controller: function($scope, $element) {
+            controller: function($scope, $element, SongData) {
                 $scope.audio = new Audio();
                 $scope.currentNum = 0;
                 $scope.info = '';
                 $scope.showArtist = false;
                 // var _ctrl = this;
-                    
 
                 // tell others to give me my prev/next track (with audio.set message)
                 $scope.next = function(){ $rootScope.$broadcast('audio.next'); };
@@ -21,13 +20,8 @@ angular.module('audioPlayer-directive', [])
 
                 //Interrobang
                 $scope.interrobang = function(){ 
-                    
                     $scope.showArtist = !$scope.showArtist;
-                    // $scope.showArtist = true;
-                    // console.log(_ctrl.showArtist);
-                    // $scope.$broadcast('interrobang', $scope.toggle);
                 };
-                // $scope.time = function(){ $rootScope.$$broadcast('audio.tiempo', $scope.audio.timeupdate()) };
 
                 // listen for audio-element events, and broadcast stuff
                 $scope.audio.addEventListener('play', function(){ $rootScope.$broadcast('audio.play', this); });
@@ -36,11 +30,12 @@ angular.module('audioPlayer-directive', [])
                 $scope.audio.addEventListener('ended', function(){ $rootScope.$broadcast('audio.ended', this); $scope.next(); });
 
                 // respond to set track messsages
-                $rootScope.$on('audio.set', function(r, file, info, currentNum, totalNum){
+                $rootScope.$on('audio.set', function(r, imgRoot, currentNum, totalNum){
+                    var currentSong = SongData.getSong(currentNum);
                     var playing = !$scope.audio.paused;
-                    $scope.audio.src = file;
+                    $scope.audio.src = imgRoot + currentSong.file;
                     var a = playing ? $scope.audio.play() : $scope.audio.pause();
-                    $scope.info = info;
+                    $scope.info = currentSong;
                     $scope.currentNum = currentNum;
                     $scope.totalNum = totalNum;
                     console.log(totalNum);
